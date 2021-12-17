34a23cc228d2bcfc180a49e2e2dc915a8034c9d6
jiyong <pjayk12@gmail.com>
2021-12-18 00:32:01 +0900

    12.18

--- src/main/java/com/example/springboot/web/IndexController.java
+++ src/main/java/com/example/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.example.springboot.web;
 
+import com.example.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
@@ -15,4 +22,5 @@ public class IndexController {
     public String postsSave(){
         return "posts-save";
     }
+
 }
